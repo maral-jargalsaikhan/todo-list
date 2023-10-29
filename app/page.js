23df1530737c51d51892f845de9c04b93d7bddf3
Maral Jargalsaikhan <maral.j@m-bank.mn>
2023-10-30 02:05:49 +0800

    add function for get todo list from fe

--- app/page.js
+++ app/page.js
@@ -1,6 +1,21 @@
 import TodoList from "./components/TodoList";
 
-export default function Home() {
+const API_URL = process.env.API_URL;
+
+const getTodoList = async () => {
+  try {
+    const response = await fetch(`${API_URL}/api/todos`, {
+      cache: "no-store",
+    });
+    return response.json();
+  } catch (error) {
+    console.log("Error on Loading todos: ", error);
+  }
+};
+
+export default async function Home() {
+  const { todos } = await getTodoList();
+
   return (
     <main>
       <h1>Demo for Todo List App</h1>
