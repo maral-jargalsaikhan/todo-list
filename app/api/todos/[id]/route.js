f833005d7c8f68f472c3baa42a177abf20369cc0
Maral Jargalsaikhan <maral.j@m-bank.mn>
2023-10-30 01:55:45 +0800

    add api GET, PUT functions for find by ID

---
+++ app/api/todos/[id]/route.js
@@ -0,0 +1,24 @@
+import connectMongoDB from "@/lib/connectMongoDB";
+import Todo from "@/schema/Todo";
+import { NextResponse } from "next/server";
+
+export async function GET(request, { params }) {
+  const { id } = params;
+  await connectMongoDB();
+  const todo = await Todo.findOne({ _id: id });
+  return NextResponse.json({ todo }, { status: 200 });
+}
+
+export async function PUT(request, { params }) {
+  const { id } = params;
+  const {
+    newTodo: todo,
+    newCategory: category,
+    newDeadline: deadline,
+  } = await request.json();
+
+  await connectMongoDB();
+  await Todo.findByIdAndUpdate(id, { todo, category, deadline });
+
+  return NextResponse.json({ message: "Todo updated" }, { status: 200 });
+}
